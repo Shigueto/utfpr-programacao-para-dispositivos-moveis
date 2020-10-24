8216dbce76b2cd64e21e0362592035402d8e00b3
João Guilherme Brasil Pichetti <joaopichetti@Mac-mini.local>
2020-10-24 14:47:50 -0300

    Internacionalizando aplicação "Calculadora"

--- Calculadora/app/src/main/java/br/edu/utfpr/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/br/edu/utfpr/calculadora/MainActivity.java
@@ -1,6 +1,7 @@
 package br.edu.utfpr.calculadora;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,6 +9,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     EditText edtValor1;
@@ -28,8 +33,7 @@ public class MainActivity extends AppCompatActivity {
         btnSomar.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                Toast.makeText(MainActivity.this,
-                        "Esse botão serve para somar dois valores",
+                Toast.makeText(MainActivity.this, R.string.sobre_somar,
                         Toast.LENGTH_LONG).show();
                 return true; // eventos subsequentes não serão executados
             }
@@ -38,17 +42,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void onSomarClick(View v) {
         try {
+            Locale locale = Locale.getDefault();
+            Log.d("TESTE", "Linguagem: " + locale.getLanguage());
+            Log.d("TESTE", "País: " + locale.getCountry());
+            NumberFormat nf = NumberFormat.getNumberInstance(locale);
             // Entrada de dados
-            Double valor1 = Double.parseDouble(edtValor1.getText().toString());
-            Double valor2 = Double.parseDouble(edtValor2.getText().toString());
+            Double valor1 = nf.parse(edtValor1.getText().toString()).doubleValue();
+            Double valor2 = nf.parse(edtValor2.getText().toString()).doubleValue();
             // Processamento
             Double soma = valor1 + valor2;
             // Saída
-            txtResultado.setText(String.valueOf(soma));
-        } catch (NumberFormatException ex) {
+            txtResultado.setText(nf.format(soma));
+        } catch (ParseException ex) {
             ex.printStackTrace();
-            Toast toast = Toast.makeText(this,
-                    "Informe valores válidos",
+            Toast toast = Toast.makeText(this, R.string.valores_invalidos,
                     Toast.LENGTH_LONG);
             toast.show();
         }
@@ -57,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
     public void onLimparClick(View v) {
         edtValor1.getText().clear();
         edtValor2.getText().clear();
-        txtResultado.setText("0.0");
+        txtResultado.setText(R.string.zeros);
         edtValor1.requestFocus();
     }
 }
